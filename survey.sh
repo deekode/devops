98d4ddbc4f4d747972d339f5746ccba05f306a23
Chukwudike Uwanaka <dikky@Chukwudikes-MacBook-Pro.local>
2022-06-09 23:24:08 +0100

    new commit

--- survey.sh
+++ survey.sh
@@ -5,7 +5,11 @@
 #
 #######################################
 
-read -p "What is your name ? " name
+   name="Default"
+   choice=1
+   answer=1
+
+if [ "$1" = "-y" ]; then name="Default" ; else read -p "What is your name ? " name; fi
 
 function thanks () {
     echo "Thank you $name for taking this survey! We will get back to you."
@@ -14,21 +18,25 @@ function thanks () {
 
 
 function aws () {
-  printf "Which AWS service do you use? \n 1. EC2 \n 2. EBS \n 3. S3 \n "
-  read -p "" answer
+ 
+  printf "Which AWS service do you use? \n 1. EC2 \n 2. EBS \n 3. S3 \n"
+
+  if [ "$1" = "-y" ]; then answer=$answer ; else read -p read -p "" answer; fi
+
   thanks "$answer"
 
 }
 
 function azure () {
- printf "Which Azure service do you use? \n 1. AKS \n 2. Azure SQL \n 3. Azure Virtual Machines \n "
-read -p "" answer
+printf "Which Azure service do you use? \n 1. AKS \n 2. Azure SQL \n 3. Azure Virtual Machines \n "
+if [ "$1" = "-y" ]; then answer=$answer ; else read -p read -p "" answer; fi
   thanks "$answer"
 }
 
 function gcp () {
+
 printf "Which GCP service do you use? \n 1. Compute Engine \n 2. Cloud Storage \n 3. Cloud SQL \n "
-read -p "" answer
+if [ "$1" = "-y" ]; then answer=$answer ; else read -p read -p "" answer; fi
 thanks "$answer"
 }
 
@@ -38,16 +46,16 @@ while true
 
 do
   printf "Which Cloud Provider do you use ? \n 1. AWS \n 2. Azure \n 3. GCP \n "
-  read -p "" choice
+  if [ "$1" = "-y" ]; then  choice="$choice"; else read -p "" choice; fi
   if [ "$choice" -eq 1 ]
  then 
-   aws
+   aws "$1"
 elif [ "$choice" -eq 2 ]
 then
-    azure
+    azure "$1"
 elif [ "$choice" -eq 3 ]
 then
-    gcp
+    gcp "$1"
 fi
   
 done
