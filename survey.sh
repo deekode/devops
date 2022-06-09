a09ba8fccea45d73de07dbfd636656842973046b
Chukwudike Uwanaka <dikky@Chukwudikes-MacBook-Pro.local>
2022-06-09 10:09:13 +0100

    Removed all the echo messages

--- survey.sh
+++ survey.sh
@@ -2,7 +2,7 @@
 
 
 #######################################
-# Simple Survey Script
+#
 #######################################
 
 read -p "What is your name ? " name
@@ -14,32 +14,22 @@ function thanks () {
 
 
 function aws () {
-  echo "Which AWS service do you use?"
-  echo "1. EC2"
-  echo "2. EBS"
-  echo "3. S3"
+  printf "Which AWS service do you use? \n 1. EC2 \n 2. EBS \n 3. S3 \n "
   read -p "" answer
-
   thanks "$answer"
 
 }
 
 function azure () {
-  echo "Which Azure service do you use?"
-  echo "1. AKS"
-  echo "2. Azure SQL"
-  echo "3. Azure Virtual Machines"
-
+ printf "Which Azure service do you use? \n 1. AKS \n 2. Azure SQL \n 3. Azure Virtual Machines \n "
+read -p "" answer
   thanks "$answer"
 }
 
 function gcp () {
-  echo "Which GCP service do you use?"
-  echo "1. Compute Engine"
-  echo "2. Cloud Storage"
-  echo "3. Cloud SQL"
-
- thanks "$answer"
+printf "Which GCP service do you use? \n 1. Compute Engine \n 2. Cloud Storage \n 3. Cloud SQL \n "
+read -p "" answer
+thanks "$answer"
 }
 
 
@@ -47,11 +37,7 @@ while true
 
 
 do
-  echo "Which Cloud Provider do you use ?"
-  echo "1. AWS"
-  echo "2. Azure"
-  echo "3. GCP"
-  
+  printf "Which Cloud Provider do you use ? \n 1. AWS \n 2. Azure \n 3. GCP \n "
   read -p "" choice
   if [ "$choice" -eq 1 ]
  then 
